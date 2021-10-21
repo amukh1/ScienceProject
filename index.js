a3b108dbbd6264dbf7ec768ed7d07e4fa11cb3d1
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-10-21 13:34:05 -0400

    aaa

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let q1 = 'What system are we doing?'
 let q2 = 'QUESTION NUMBER TWOOOOOO'
 let q3 = 'question 3'
+let q4 = 'What system are we doing?'
+let q5 = 'QUESTION NUMBER TWOOOOOO'
+let q6 = 'question 3'
+let q7 = 'What system are we doing?'
+let q8 = 'QUESTION NUMBER TWOOOOOO'
+let q9 = 'question 3'
+let q10 = 'What system are we doing?'
+let q11 = 'QUESTION NUMBER TWOOOOOO'
+let q12 = 'question 3'
+let q13 = 'What system are we doing?'
+let q14 = 'QUESTION NUMBER TWOOOOOO'
+let q15 = 'question 3'
 
 
 let q1o1 = 'Skeletal'
@@ -10,6 +22,54 @@ let q1o3 = 'Incorrect'
 let q2o1 = 'Incorrect'
 let q2o2 = 'Correct'
 let q2o3 = 'Incorrect'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'corr'
+let q3o2 = 'Incorrect'
+let q3o3 = 'notCorrect'
+
+let q3o1 = 'Ineorrect'
+let q3o2 = 'cor'
+let q3o3 = 'nothteCorrect'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
+
+let q3o1 = 'Incorrect'
+let q3o2 = 'Incorrect'
+let q3o3 = 'Correct'
 
 let q3o1 = 'Incorrect'
 let q3o2 = 'Incorrect'
@@ -43,6 +103,78 @@ if(stage == 1){
     o2.innerHTML = q3o2
     o3.innerHTML = q3o3
     correct = 'three'
+}else if(stage == 4){
+    question.innerHTML = q4
+    o1.innerHTML = q4o1
+    o2.innerHTML = q4o2
+    o3.innerHTML = q4o3
+    correct = 'three'
+}else if(stage == 5){
+    question.innerHTML = q5
+    o1.innerHTML = q5o1
+    o2.innerHTML = q5o2
+    o3.innerHTML = q5o3
+    correct = 'three'
+}else if(stage == 6){
+    question.innerHTML = q6
+    o1.innerHTML = q6o1
+    o2.innerHTML = q6o2
+    o3.innerHTML = q6o3
+    correct = 'three'
+}else if(stage == 7){
+    question.innerHTML = q7
+    o1.innerHTML = q7o1
+    o2.innerHTML = q7o2
+    o3.innerHTML = q7o3
+    correct = 'three'
+}else if(stage == 8){
+    question.innerHTML = q8
+    o1.innerHTML = q8o1
+    o2.innerHTML = q8o2
+    o3.innerHTML = q8o3
+    correct = 'three'
+}else if(stage == 9){
+    question.innerHTML = q9
+    o1.innerHTML = q9o1
+    o2.innerHTML = q9o2
+    o3.innerHTML = q9o3
+    correct = 'three'
+}else if(stage == 10){
+    question.innerHTML = q10
+    o1.innerHTML = q10o1
+    o2.innerHTML = q10o2
+    o3.innerHTML = q10o3
+    correct = 'three'
+}else if(stage == 11){
+    question.innerHTML = q4
+    o1.innerHTML = q4o1
+    o2.innerHTML = q4o2
+    o3.innerHTML = q4o3
+    correct = 'three'
+}else if(stage == 12){
+    question.innerHTML = q12
+    o1.innerHTML = q12o1
+    o2.innerHTML = q12o2
+    o3.innerHTML = q12o3
+    correct = 'three'
+}else if(stage == 13){
+    question.innerHTML = q13
+    o1.innerHTML = q13o1
+    o2.innerHTML = q13o2
+    o3.innerHTML = q13o3
+    correct = 'three'
+}else if(stage == 14){
+    question.innerHTML = q14
+    o1.innerHTML = q14o1
+    o2.innerHTML = q14o2
+    o3.innerHTML = q14o3
+    correct = 'three'
+}else if(stage == 15){
+    question.innerHTML = q15
+    o1.innerHTML = q15o1
+    o2.innerHTML = q15o2
+    o3.innerHTML = q15o3
+    correct = 'three'
 }
 
 
@@ -70,12 +202,82 @@ function answerOne() {
         o3.innerHTML = q3o3
         correct = 'three'
     }else if(stage == 4){
-        window.location.href = "https://amukh1.dev/ending.html";
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 5){
+        question.innerHTML = q5
+        o1.innerHTML = q5o1
+        o2.innerHTML = q5o2
+        o3.innerHTML = q5o3
+        correct = 'three'
+    }else if(stage == 6){
+        question.innerHTML = q6
+        o1.innerHTML = q6o1
+        o2.innerHTML = q6o2
+        o3.innerHTML = q6o3
+        correct = 'three'
+    }else if(stage == 7){
+        question.innerHTML = q7
+        o1.innerHTML = q7o1
+        o2.innerHTML = q7o2
+        o3.innerHTML = q7o3
+        correct = 'three'
+    }else if(stage == 8){
+        question.innerHTML = q8
+        o1.innerHTML = q8o1
+        o2.innerHTML = q8o2
+        o3.innerHTML = q8o3
+        correct = 'three'
+    }else if(stage == 9){
+        question.innerHTML = q9
+        o1.innerHTML = q9o1
+        o2.innerHTML = q9o2
+        o3.innerHTML = q9o3
+        correct = 'three'
+    }else if(stage == 10){
+        question.innerHTML = q10
+        o1.innerHTML = q10o1
+        o2.innerHTML = q10o2
+        o3.innerHTML = q10o3
+        correct = 'three'
+    }else if(stage == 11){
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 12){
+        question.innerHTML = q12
+        o1.innerHTML = q12o1
+        o2.innerHTML = q12o2
+        o3.innerHTML = q12o3
+        correct = 'three'
+    }else if(stage == 13){
+        question.innerHTML = q13
+        o1.innerHTML = q13o1
+        o2.innerHTML = q13o2
+        o3.innerHTML = q13o3
+        correct = 'three'
+    }else if(stage == 14){
+        question.innerHTML = q14
+        o1.innerHTML = q14o1
+        o2.innerHTML = q14o2
+        o3.innerHTML = q14o3
+        correct = 'three'
+    }else if(stage == 15){
+        question.innerHTML = q15
+        o1.innerHTML = q15o1
+        o2.innerHTML = q15o2
+        o3.innerHTML = q15o3
+        correct = 'three'
     }
 
     
     
-    }else {alert('incorrect')}
+    }
 }
 
 function answerTwo() {
@@ -102,12 +304,82 @@ function answerTwo() {
         o3.innerHTML = q3o3
         correct = 'three'
     }else if(stage == 4){
-        window.location.href = "http://127.0.0.1:5500/ending.html";
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 5){
+        question.innerHTML = q5
+        o1.innerHTML = q5o1
+        o2.innerHTML = q5o2
+        o3.innerHTML = q5o3
+        correct = 'three'
+    }else if(stage == 6){
+        question.innerHTML = q6
+        o1.innerHTML = q6o1
+        o2.innerHTML = q6o2
+        o3.innerHTML = q6o3
+        correct = 'three'
+    }else if(stage == 7){
+        question.innerHTML = q7
+        o1.innerHTML = q7o1
+        o2.innerHTML = q7o2
+        o3.innerHTML = q7o3
+        correct = 'three'
+    }else if(stage == 8){
+        question.innerHTML = q8
+        o1.innerHTML = q8o1
+        o2.innerHTML = q8o2
+        o3.innerHTML = q8o3
+        correct = 'three'
+    }else if(stage == 9){
+        question.innerHTML = q9
+        o1.innerHTML = q9o1
+        o2.innerHTML = q9o2
+        o3.innerHTML = q9o3
+        correct = 'three'
+    }else if(stage == 10){
+        question.innerHTML = q10
+        o1.innerHTML = q10o1
+        o2.innerHTML = q10o2
+        o3.innerHTML = q10o3
+        correct = 'three'
+    }else if(stage == 11){
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 12){
+        question.innerHTML = q12
+        o1.innerHTML = q12o1
+        o2.innerHTML = q12o2
+        o3.innerHTML = q12o3
+        correct = 'three'
+    }else if(stage == 13){
+        question.innerHTML = q13
+        o1.innerHTML = q13o1
+        o2.innerHTML = q13o2
+        o3.innerHTML = q13o3
+        correct = 'three'
+    }else if(stage == 14){
+        question.innerHTML = q14
+        o1.innerHTML = q14o1
+        o2.innerHTML = q14o2
+        o3.innerHTML = q14o3
+        correct = 'three'
+    }else if(stage == 15){
+        question.innerHTML = q15
+        o1.innerHTML = q15o1
+        o2.innerHTML = q15o2
+        o3.innerHTML = q15o3
+        correct = 'three'
     }
     
     
 
-    }else {alert('incorrect')}
+    }
 }
 
 function answerThree() {
@@ -134,10 +406,80 @@ function answerThree() {
         o3.innerHTML = q3o3
         correct = 'three'
     }else if(stage == 4){
-        window.location.href = "https://science.amukh1.dev/ending.html";
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 5){
+        question.innerHTML = q5
+        o1.innerHTML = q5o1
+        o2.innerHTML = q5o2
+        o3.innerHTML = q5o3
+        correct = 'three'
+    }else if(stage == 6){
+        question.innerHTML = q6
+        o1.innerHTML = q6o1
+        o2.innerHTML = q6o2
+        o3.innerHTML = q6o3
+        correct = 'three'
+    }else if(stage == 7){
+        question.innerHTML = q7
+        o1.innerHTML = q7o1
+        o2.innerHTML = q7o2
+        o3.innerHTML = q7o3
+        correct = 'three'
+    }else if(stage == 8){
+        question.innerHTML = q8
+        o1.innerHTML = q8o1
+        o2.innerHTML = q8o2
+        o3.innerHTML = q8o3
+        correct = 'three'
+    }else if(stage == 9){
+        question.innerHTML = q9
+        o1.innerHTML = q9o1
+        o2.innerHTML = q9o2
+        o3.innerHTML = q9o3
+        correct = 'three'
+    }else if(stage == 10){
+        question.innerHTML = q10
+        o1.innerHTML = q10o1
+        o2.innerHTML = q10o2
+        o3.innerHTML = q10o3
+        correct = 'three'
+    }else if(stage == 11){
+        question.innerHTML = q4
+        o1.innerHTML = q4o1
+        o2.innerHTML = q4o2
+        o3.innerHTML = q4o3
+        correct = 'three'
+    }else if(stage == 12){
+        question.innerHTML = q12
+        o1.innerHTML = q12o1
+        o2.innerHTML = q12o2
+        o3.innerHTML = q12o3
+        correct = 'three'
+    }else if(stage == 13){
+        question.innerHTML = q13
+        o1.innerHTML = q13o1
+        o2.innerHTML = q13o2
+        o3.innerHTML = q13o3
+        correct = 'three'
+    }else if(stage == 14){
+        question.innerHTML = q14
+        o1.innerHTML = q14o1
+        o2.innerHTML = q14o2
+        o3.innerHTML = q14o3
+        correct = 'three'
+    }else if(stage == 15){
+        question.innerHTML = q15
+        o1.innerHTML = q15o1
+        o2.innerHTML = q15o2
+        o3.innerHTML = q15o3
+        correct = 'three'
     }
 
     
     
-    }else {alert('incorrect')}
+    }
 }
